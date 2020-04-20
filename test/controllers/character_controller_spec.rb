b475d952500dfce03539412b7c54a2cff847ded5
Justin Linville <justinlinville@Justins-iMac.local>
2020-04-20 12:52:10 -0700

    adds character controller tests

---
+++ test/controllers/character_controller_spec.rb
@@ -0,0 +1,13 @@
+RSpec.describe CharacterController, :type => :controller do
+  describe "Get index" do
+    it "renders the index template" do
+      get :index
+      expect(response).to render_template("index")
+    end
+
+    it "has a 200 status code" do
+      get :index
+      expect(response.status).to eq(200)
+    end
+  end
+end
