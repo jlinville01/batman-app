fa3c1c29ed8d4e4ebaa9ddd5d289362c5f70a546
Justin Linville <justinlinville@Justins-iMac.local>
2020-04-21 09:51:25 -0700

    Adds more character unit tests

--- spec/unit/controller/character_controller_spec.rb
+++ spec/unit/controller/character_controller_spec.rb
@@ -1,5 +1,5 @@
 RSpec.describe CharactersController, type: :controller do
-  describe 'GET index' do
+  describe '.index' do
     it 'renders :index' do
       get :index
       expect(response).to render_template :index
@@ -8,6 +8,40 @@ RSpec.describe CharactersController, type: :controller do
     it 'has a 200 status code' do
       get :index
       expect(response.status).to eq(200)
+    end
+  end
+
+  describe '.new' do
+    it 'when character does not exist, successfully render' do
+      get :new, params: { alias: 'Red Hood', real_name: 'Jason Todd', debut: 'Never', status: 'Hero', bio: 'a vigilante anti-hero who has been a member of and antagonist to the Batman Family. Batman originally trained him to be the second Robin, his new side-kick after Dick Grayson became Nightwing. Jason was murdered by the Joker, although he was later resurrected and returned as the second Red Hood.' }
+
+      expect(response).to render_template :new
+    end
+
+    it 'has a 200 status code' do
+      get :new, params: { alias: 'Red Hood', real_name: 'Jason Todd', debut: 'Never', status: 'Hero', bio: 'a vigilante anti-hero who has been a member of and antagonist to the Batman Family. Batman originally trained him to be the second Robin, his new side-kick after Dick Grayson became Nightwing. Jason was murdered by the Joker, although he was later resurrected and returned as the second Red Hood.' }
+
+      expect(response.status).to eq(200)
+    end
+  end
+
+  describe '.create' do
+    def valid_params
+      {
+        character: {
+          alias: 'Red Hood',
+          real_name: 'Jason Todd',
+          debut: 'Never',
+          status: 'Hero',
+          bio: 'A vigilante anti-hero who has been a member of and antagonist to the Batman Family. Batman originally trained him to be the second Robin, his new side-kick after Dick Grayson became Nightwing. Jason was murdered by the Joker, although he was later resurrected and returned as the second Red Hood.'
+        }
+      }
+    end
+
+    it 'create a credit with seller id' do
+      post :create, params: valid_params
+
+      expect(Character.last.alias).to eq('Red Hood')
     end
   end
 end
