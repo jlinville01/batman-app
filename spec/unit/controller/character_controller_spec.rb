3c8cc83ac613ec88dda061ee828a3d7a052da128
Justin Linville <justinlinville@Justins-iMac.local>
2020-04-21 08:53:17 -0700

    adds controller unit tests

---
+++ spec/unit/controller/character_controller_spec.rb
@@ -0,0 +1,13 @@
+RSpec.describe CharactersController, type: :controller do
+  describe 'GET index' do
+    it 'renders :index' do
+      get :index
+      expect(response).to render_template :index
+    end
+
+    it 'has a 200 status code' do
+      get :index
+      expect(response.status).to eq(200)
+    end
+  end
+end
