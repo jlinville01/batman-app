7efbafba2954b54fa8decc84e69dce1c9bfd0333
Justin Linville <justinlinville@Justins-iMac.local>
2020-04-24 16:14:40 -0700

    removes avatar

--- app/controllers/api/v1/characters_controller.rb
+++ app/controllers/api/v1/characters_controller.rb
@@ -25,7 +25,7 @@ module Api
       private
 
       def character_params
-        params.permit(:avatar, :alter_ego, :real_name, :debut, :status, :bio)
+        params.permit(:alter_ego, :real_name, :debut, :status, :bio)
       end
     end
   end
